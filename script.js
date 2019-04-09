23c3d8af7881c204521ce96bd818988e7363e09d
Will <willagodu@Wills-MacBook-Pro.local>
2019-04-09 14:21:56 -0400

    a short message describing your changes

---
+++ script.js
@@ -0,0 +1,37 @@
+var button = document.getElementById('change')
+var mode = window.localStorage.getItem('value')
+var visit = window.localStorage.getItem('times')
+if (visit === null) {
+  visit = 0
+  window.localStorage.setItem('times', visit)
+}
+var visitInt = parseInt(visit)
+visitInt += 1
+visit = visitInt
+window.localStorage.setItem('times', visit)
+document.write(visit)
+if (mode === null) {
+  mode = 'true'
+  document.body.style.backgroundColor = 'white'
+  document.body.style.color = 'black'
+} else if (mode === 'true') {
+  document.body.style.backgroundColor = 'white'
+  document.body.style.color = 'black'
+} else if (mode === 'false') {
+  document.body.style.backgroundColor = 'black'
+  document.body.style.color = 'white'
+}
+
+button.addEventListener('click', function () {
+  if (mode === 'false') {
+    mode = 'true'
+    window.localStorage.setItem('value', mode)
+    document.body.style.backgroundColor = 'white'
+    document.body.style.color = 'black'
+  } else if (mode === 'true') {
+    mode = 'false'
+    window.localStorage.setItem('value', mode)
+    document.body.style.backgroundColor = 'black'
+    document.body.style.color = 'white'
+  }
+})
